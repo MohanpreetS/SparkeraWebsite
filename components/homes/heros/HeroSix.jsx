3172ec6169d9c31f1cfb12ddcb5029a4ddc8d0fd
Mohanpreet <msnarang@uwaterloo.ca>
2024-02-24 13:09:03 -0500

    checking text error

--- components/homes/heros/HeroSix.jsx
+++ components/homes/heros/HeroSix.jsx
@@ -60,7 +60,7 @@ export default function HeroSix() {
                   Spark your child's Curiosity
                 </h1> */}
                 <h1 className="masthead__title">
-                  <span className="concert">Spark</span>{" "}Your <br />
+                  <span className="concert gradient-text">Spark</span>{" "}Your <br />
                   Child's <span className="gradient-text">Curiosity</span>
                 </h1>
                 <h2 className="semi-title"><span className="text-purple-1">Math</span> and <span className="text-purple-1">English</span> Worksheet Based Program</h2>
